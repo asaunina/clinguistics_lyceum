2a223af4d5eaa9ba54394957deb8d8af4022d8ba
asaunina <72650924+asaunina@users.noreply.github.com>
2020-10-16 20:02:51 +0300

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,34 @@
+while True:
+    a = input()
+    b = input()
+    true1 = 0
+    true2 = 0
+    true3 = 0
+    true4 = 0
+    for i in range (len(a)):
+        if "0" <= a[i] <= "9":
+            print("Wrong answer")
+            break
+        else:
+            if "a" <= a[i] <= "z" or "A" <= a[i] <= "Z":
+                true1 = true1 + 1
+            elif "а" <= a[i] <= "я" or "А" <= a[i] <= "Я":
+                true2 = true2 + 1
+    for i in range (len(b)):
+        if "0" <= b[i] <= "9":
+            print("Wrong answer")
+            break
+        else:
+            if "a" <= b[i] <= "z" or "A" <= b[i] <= "Z":
+                true3 = true3 + 1
+            elif "а" <= b[i] <= "я" or "А" <= b[i] <= "Я":
+                true4 = true4 + 1
+    if true1 == len(a) and true4 == len(b):
+        print("Русское слово -", b)
+        print("The English word is", a)
+    elif true2 == len(a) and true3 == len(b):
+        print("Русское слово -", a)
+        print("The English word is", b)
+    else:
+        print("Wrong answer")
+    break
